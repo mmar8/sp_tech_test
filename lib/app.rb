a95957ccf4b4d6a61d3801cd936ac8d6fcaba041
Maria Vanaale <mvanaale@gmail.com>
2019-05-20 14:49:20 +0100

    can display the list of webpages

--- lib/app.rb
+++ lib/app.rb
@@ -1,2 +1,8 @@
 class Webpages
+  def read_list
+    @webpages = []
+    File.readlines('webserver.log').each do |line|
+      @webpages << line.split
+    end
+  end
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -2,6 +2,8 @@ require 'app.rb'
 
 describe Webpages do
   it 'can display the list of webpages' do
-    
+    webpages = Webpages.new
+    subject.read_list
+    expect(webpages.read_list).to include("/help_page/1 126.318.035.038\n")
   end
 end
