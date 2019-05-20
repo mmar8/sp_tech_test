ee4d61c1ffb76695af97220497b27b4b6b207551
Maria Vanaale <mvanaale@gmail.com>
2019-05-20 15:35:01 +0100

    code refactored and tests refactored

--- lib/app.rb
+++ lib/app.rb
@@ -2,13 +2,26 @@ class Webpages
   def initialize
     @webpages = []
   end
-
+  def run
+    read_list
+    p display_sort_by_most_page_views
+    p display_sort_by_most_unique_views
+  end
   def read_list
     File.readlines('webserver.log').each do |line|
       @webpages << line.split
     end
   end
 
+  def display_sort_by_most_page_views
+    sort_by_most_page_views
+  end
+
+  def display_sort_by_most_unique_views
+    sort_by_most_unique_views
+  end 
+
+  private 
   def sort_by_most_page_views
     @webpages.group_by(&:first).map{|k,v| [k, v.count]}.sort.reverse!.flatten
   end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -10,13 +10,17 @@ describe Webpages do
   it 'sorts webpages by most page views' do
     webpages = Webpages.new
     webpages.read_list
-    expect(webpages.sort_by_most_page_views).to include("/index", 82)
+    expect(webpages.display_sort_by_most_page_views).to include("/index", 82)
   end
 
   it 'sorts webpages by most unique views' do
     webpages = Webpages.new
     webpages.read_list
-    expect(webpages.sort_by_most_unique_views).to include("/index", 23)
+    expect(webpages.display_sort_by_most_unique_views).to include("/index", 23)
+  end
+
+  it 'runs app' do
+    expect {subject.run }.not_to raise_error
   end
         
 end
